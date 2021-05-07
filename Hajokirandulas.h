76b233fc841bb38d13134def80e77e08a1048538
Lehelbalint <79191891+Lehelbalint@users.noreply.github.com>
2021-05-07 14:52:22 +0300

    Add files via upload

---
+++ Hajokirandulas.h
@@ -0,0 +1,8 @@
+//
+// Created by lehel on 5/7/2021.
+//
+
+#ifndef LAB10_HAJOKIRANDULAS_H
+#define LAB10_HAJOKIRANDULAS_H
+
+#endif //LAB10_HAJOKIRANDULAS_H
--- main.c
+++ main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include "keresofa.h"
 
 int main() {
-    printf("Hello!\n");
-    return 0;
+    Node *root;
+    root=NULL;
+    insert(&root,27);
+    insert(&root,14);
+    insert(&root,35);
+    insert(&root,10);
+    insert(&root,19);
+    insert(&root,31);
+    insert(&root,42);
+    inorder(root);
+    printf("A legkisebb elem: %i\n",min(root));
+    printf("A legnagyobb elem elem: %i\n",max(root));
+    //root=deleteNode(root,14);
+    printf("\n Inorder bejaras: \n");
+    inorder(root);
+    printf("\n Preoder bejaras: \n");
+    preorder(root);
+    printf("\n Postorder bejaras: \n");
+    postorder(root);
+    printf("\n %i ",prev(root,15));
+    printf("\n %i ",next(root,15));
+
+
+
+
+
+
 }
